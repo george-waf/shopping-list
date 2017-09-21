03471f2136a5811884feb35ead4000d49a5e07f8
stankim <stankim.ks@gmail.com>
2017-09-22 01:15:36 +0300

    (chore) changed app file name to suit gunicorn command

--- app.py
+++ app.py
@@ -2,23 +2,23 @@
 from flask import Flask
 from flask import render_template
 
-APP = Flask(__name__)
+app = Flask(__name__)
 
-@APP.route('/')
-@APP.route('/sign-up')
+@app.route('/')
+@app.route('/sign-up')
 def index():
     '''rendering the sign up page'''
     return render_template('sign-up.html')
 
-@APP.route('/login')
+@app.route('/login')
 def login():
     '''Rendering the login page'''
     return render_template('login.html')
 
-@APP.route('/dashboard')
+@app.route('/dashboard')
 def dashboard():
     '''Rendering the dashboard page'''
     return render_template('dashboard.html')
 
 if __name__ == "__main__":
-    APP.run()
+    app.run()
