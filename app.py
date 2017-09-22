13affe9fc8cb557e1e685fdc98f508032b6b9fda
stankim <stankim.ks@gmail.com>
2017-09-22 09:57:42 +0300

    (feature) user can sign up

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 '''docstring for app'''
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/sign-up')
+@app.route('/sign-up', methods=['POST', 'GET'])
 def index():
     '''rendering the sign up page'''
-    return render_template('sign-up.html')
+    error = None
+    if request.method == 'POST':
+        if validate_data(request.form['username'], request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid format of username or password'
+    return render_template('sign-up.html', error=error)
 
 @app.route('/login')
 def login():
     '''Rendering the login page'''
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'username or password not correct'
+    return render_template('login.html', error=error)
 
 @app.route('/dashboard')
 def dashboard():
