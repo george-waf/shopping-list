73dfcceb02fe804458be0a411f4586fccd809972
stankim <stankim.ks@gmail.com>
2017-09-22 11:14:52 +0300

    (chore) added unittests for the root route, login and sign-up routes

--- tests/test-app.py
+++ tests/test-app.py
@@ -1,9 +1,47 @@
 import unittest
+import app
  
 class TddInShoppist(unittest.TestCase):
- 
-    def test_that_something_happens(self):
-        pass
+    def setUp(self):
+        '''Setup the app for testing'''
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_get_root_route(self):
+        '''Test access to the GET '/' route'''
+        response = self.app.get('/')
+        #test if it returns something
+        self.assertEqual(str(response), "<Response streamed [200 OK]>")
+
+    def test_get_sign_up(self):
+        '''Test access to the GET 'sign-up/' route'''
+        response = self.app.get('/sign-up')
+        #test if it returns something
+        self.assertEqual(str(response), "<Response streamed [200 OK]>")
+
+    def test_get_login(self):
+        '''Test access to the GET 'sign-up/' route'''
+        response = self.app.get('/login')
+        #test if it returns something
+        self.assertEqual(str(response), "<Response streamed [200 OK]>")
+
+    def  test_post_sign_up(self):
+        '''Test if user can sign up'''
+        response = self.app.post('/sign-up', data=dict(
+            email="user2@shoppist.com",
+            password="123456"
+            ), follow_redirects=True)
+        #test if it returns something
+        self.assertEqual(str(response), "<Response streamed [200 OK]>")
+
+    def  test_post_login(self):
+        '''Test if user can login'''
+        response = self.app.post('/login', data=dict(
+            email="user@shoppist.com",
+            password="qwerty"
+            ), follow_redirects=True)
+        #test if it returns something
+        self.assertEqual(str(response), "<Response streamed [200 OK]>")
 
 if __name__ == '__main__':
     unittest.main()
