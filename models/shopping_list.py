b4ca6c10690b154138de9ee67978a58cd09ad8e3
stankim <stankim.ks@gmail.com>
2017-09-28 14:31:44 +0300

    (feature) add auto ids to shopping lists

--- models/shopping_list.py
+++ models/shopping_list.py
@@ -1,8 +1,22 @@
 '''module for handling shopping lists'''
+
+
 class ShoppingList(object):
     """docstring for shopping_list"""
+    next_id = 0
 
     def __init__(self, title, items, owner):
         self.title = title
         self.items = items
         self.owner = owner
+        self.id = self.get_next_id()
+
+    def get_next_id(self):
+        ShoppingList.next_id += 1
+        return ShoppingList.next_id
+
+ALL_SHOPPING_LISTS = []
+
+
+def add_shopping_list(shopping_list):
+    ALL_SHOPPING_LISTS.append(shopping_list)
